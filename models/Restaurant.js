3fd6a22b2e41d0a5e7d01c0a65d0485ee7afc633
Adarsh <adarshkumar167@gmail.com>
2023-12-10 05:22:32 +0530

    First_commit

---
+++ models/Restaurant.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const RestaurantSchema = new mongoose.Schema({
+    title:  {type: String, required: true},
+    time: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    foods: {type: Array},
+    pickup: {type: Boolean, required: false, default: true},
+    delivery: {type: Boolean, required: false, default: true},
+    owner: {type: String, required: false},
+    isAvailable: {type: Boolean, default: true},
+    code: {type: String, required: true},
+    logoUrl: {
+        type: String,
+        required: true,
+        default: 'https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png'
+    },
+    rating: {type: Number, min: 1, max: 5},
+    ratingCount: {type: String},
+    coords: {
+        id: {type: String, required: true},
+        latitude: {type: Number, required: true},
+        longitude: {type: Number, required: true},
+        latitudeDelta: {type: Number, required: true},
+        longitudeDelta: {type: Number, required: true},
+        address: {type: String, required: true},
+        title: {type: String, required: true}
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema)
