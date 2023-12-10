8b3424d55194f18a4f4d62bc02e1e5f4561965bc
Adarsh <adarshkumar167@gmail.com>
2023-12-10 22:29:59 +0530

    food_controller

--- server.js
+++ server.js
@@ -9,16 +9,20 @@ const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
 const restaurantRouter = require('./routes/restaurant')
 const categoryRouter = require('./routes/category')
+const foodRouter = require('./routes/food')
 
 
 dotenv.config()
 
+
 const admin = require('firebase-admin');
 const serviceAccount = require('./servicesAccountKey.json')
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 })
+
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('DataBase Connected')).catch((err) => console.log(err))
 
@@ -29,7 +33,7 @@ app.use('/', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/restaurant', restaurantRouter);
 app.use('/api/category', categoryRouter);
-
+app.use('/api/foods', foodRouter);
 
 
 app.listen(process.env.PORT || port, () => console.log(`Foodly backend app listening on port ${process.env.PORT}!`))
